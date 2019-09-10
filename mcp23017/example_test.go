67e423a2babaa3133fc35f216d98c137f8e3fc59
mamemomonga <mamemomonga@gmail.com>
2019-09-10 19:43:40 +0900

    MCP23017更新

--- mcp23017/example_test.go
+++ mcp23017/example_test.go
@@ -1,11 +1,16 @@
 package mcp23017_test
 
 import (
-	"github.com/mamemomonga/rpi-go-peripherals/mcp23017"
-	"os"
 	"log"
 	"time"
 	"fmt"
+
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/mamemomonga/rpi-go-peripherals/mcp23017"
+
 	"periph.io/x/periph/conn/i2c/i2creg"
 	"periph.io/x/periph/host"
 
@@ -13,6 +18,7 @@ import (
 	"periph.io/x/periph/conn/gpio"
 )
 
+// boolを文字列にする
 func bool2str(b bool) string {
 	if b {
 		return "HIGH"
@@ -20,7 +26,59 @@ func bool2str(b bool) string {
 	return "LOW"
 }
 
+// 割り込み処理
+func btnInterruptLoop(intpin gpio.PinIO, mx *mcp23017.MCP23017) {
+	fmt.Fprintln(os.Stderr,"START btnInterruptLoop")
 
+	// mx.B.Get(0) という形でも呼び出せるが
+	// mcp23017.NewPinという形でピン番号も含んだ形でインスタンス化し
+	// Get() だけで取得できるようにすることができる
+
+	// ポートB ピン0 GPB0
+	button0 := mcp23017.NewPin(mx.B, 0)
+
+	// ポートB ピン1 GPB1
+	button1 := mcp23017.NewPin(mx.B, 1)
+
+	// ポートB ピン2 GPB2
+	button2 := mcp23017.NewPin(mx.B, 2)
+
+	// ポートB ピン3 GPB3
+	button3 := mcp23017.NewPin(mx.B, 3)
+
+	for {
+		// 割り込みを待つ
+		intpin.WaitForEdge(-1)
+
+		// ポートBの値を取得
+		mx.B.Fetch()
+
+		// GPB0, GPB1, GPB2, GPB3 の値を表示
+		fmt.Fprintf(os.Stderr, " [GPB0 %4s] [GPB1 %4s] [GPB2 %4s] [GPB3 %4s]\n",
+			bool2str(button0.Get()),
+			bool2str(button1.Get()),
+			bool2str(button2.Get()),
+			bool2str(button3.Get()),
+		)
+	}
+}
+
+// LED処理
+func ledLoop(mx *mcp23017.MCP23017) {
+	fmt.Fprintln(os.Stderr,"START ledLoop")
+	for {
+		for i:=uint8(0); i<8; i++ {
+			// ポートAのピン i の LEDを点灯し、反映する
+			mx.A.Set(i,true).Apply()
+			time.Sleep( 100 * time.Millisecond )
+			// ポートAのピン i の LEDを消灯し、反映しない
+			// (次のポートAの反映の際まで遅延)
+			mx.A.Set(i,false)
+		}
+	}
+}
+
+// メイン
 func Example() {
 
 	// 初期化
@@ -44,12 +102,12 @@ func Example() {
 	mx.A.DirectionAllOutput()
 
 	// ポートAをすべてHighにし、反映する
-	mx.A.SetAllHigh(true)
+	mx.A.SetAllHigh().Apply()
 
 	time.Sleep( time.Second )
 
 	// ポートAをすべてLowにし、反映する
-	mx.A.SetAllLow(true)
+	mx.A.SetAllLow().Apply()
 
 	// ポートBをすべて入力にする
 	mx.B.DirectionAllInput()
@@ -73,42 +131,32 @@ func Example() {
 		log.Fatal(err)
 	}
 
+	fmt.Fprintln(os.Stderr," *** START ***")
+
+
 	// 割り込み処理
-	go func() {
-		for {
-			// 割り込みを待つ
-			intpin.WaitForEdge(-1)
-
-			// ポートBの値を取得
-			mx.B.Fetch()
-
-			// GPB0, GPB1, GPB2, GPB3 の値を表示
-			fmt.Fprintf(os.Stderr, " [GPB0 %4s] [GPB1 %4s] [GPB2 %4s] [GPB3 %4s]\n",
-				bool2str(mx.B.Get(0)),
-				bool2str(mx.B.Get(1)),
-				bool2str(mx.B.Get(2)),
-				bool2str(mx.B.Get(3)),
-			)
-		}
-	}()
+	go btnInterruptLoop(intpin,mx)
 
 	// LED点滅処理
-	for {
-		for i:=uint8(0); i<8; i++ {
-			// ポートAのピン i の LEDを点灯し、反映する
-			mx.A.Set(i,true,true)
-			time.Sleep( 100 * time.Millisecond )
-			// ポートAのピン i の LEDを消灯し、反映しない
-			// (次のポートAの書込の際に反映される)
-			mx.A.Set(i,false,false)
+	go ledLoop(mx)
 
-		}
-	}
+	// シグナル割り込みの設定
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGHUP, syscall.SIGINT)
 
+	// シグナルがくるまでここでブロック
+	<-quit
+
+	fmt.Fprintln(os.Stderr," *** STOP ***")
+
+	// ポートAをすべてLowにし、反映する
+	mx.A.SetAllLow().Apply()
+
+	fmt.Println("Done")
 	// testingのExample機能は標準出力を
 	// 以下の Output: の後と比較するためエラー出力に結果を出している
 	// https://golang.org/pkg/testing/#hdr-Examples
 
-	// Output: 
+	// Output: Done
 }
 
--- mcp23017/mcp23017.go
+++ mcp23017/mcp23017.go
@@ -1,4 +1,4 @@
-// mcp23017 ポートマルチプレクサ
+// mcp23017 I/Oエキスパンダ ライブラリ
 //
 package mcp23017
 
@@ -10,29 +10,6 @@ import (
 const (
 	AddrAllLow byte = 0x20 // A0,A1,A2 をすべてGNDに接続すると、アドレスは0x20
 )
-
-type MCP23017 struct {
-	dev *i2c.Dev
-	A   *PortT
-	B   *PortT
-}
-
-type PortT struct {
-	p     *MCP23017
-	addr  addrT
-	State byte
-}
-
-type addrT struct {
-	iodir   byte
-	gppu    byte
-	olat    byte
-	gpio    byte
-	gpinten byte
-	defval  byte
-	intcon  byte
-}
-
 
 // S2BV Slice to Bit vector
 func S2BV(in []uint8) uint8 {
@@ -58,6 +35,14 @@ func BV2S(in uint8) []uint8 {
 	return v
 }
 
+// MCP23017 ライブラリ
+type MCP23017 struct {
+	dev *i2c.Dev
+	A   *PortT
+	B   *PortT
+}
+
+// New 新規MCP23017 ライブラリ
 func New(bus i2c.BusCloser, addr byte) *MCP23017 {
 	t := new(MCP23017)
 	t.dev = &i2c.Dev{Addr: uint16(addr), Bus: bus}
@@ -66,12 +51,14 @@ func New(bus i2c.BusCloser, addr byte) *MCP23017 {
 	return t
 }
 
+// Write 書込
 func (t *MCP23017) Write(addr uint8, data uint8) {
 	if _,err := t.dev.Write([]byte{addr,data}); err != nil {
 		log.Fatal(err)
 	}
 }
 
+// Read 読込
 func (t *MCP23017) Read(addr uint8) byte {
 	v := make([]byte, 1,1)
 	if err := t.dev.Tx([]byte{addr},v); err != nil {
@@ -80,6 +67,25 @@ func (t *MCP23017) Read(addr uint8) byte {
 	return v[0]
 }
 
+// PortT ポート
+type PortT struct {
+	p     *MCP23017
+	addr  addrT
+	State byte
+}
+
+// addrT コマンドアドレス ICONとポート毎に異なる
+type addrT struct {
+	iodir   byte
+	gppu    byte
+	olat    byte
+	gpio    byte
+	gpinten byte
+	defval  byte
+	intcon  byte
+}
+
+// newPort 新規ポート
 func newPort(port string, parent *MCP23017) *PortT {
 	t := new(PortT)
 	t.p = parent
@@ -111,50 +117,71 @@ func newPort(port string, parent *MCP23017) *PortT {
 	return t
 }
 
-func (t *PortT) Direction(v ...uint8) {
+// Direction 方向
+func (t *PortT) Direction(v ...uint8) *PortT {
 	t.p.Write(t.addr.iodir, S2BV(v))
+	return t
 }
-func (t *PortT) DirectionAllOutput() {
+
+// DirectionAllOutput すべてのピンを出力にする
+func (t *PortT) DirectionAllOutput() *PortT {
 	t.Direction(0,0,0,0,0,0,0,0)
+	return t
 }
-func (t *PortT) DirectionAllInput() {
+
+// DirectionAllInput すべてのピンを入力にする
+func (t *PortT) DirectionAllInput() *PortT {
 	t.Direction(1,1,1,1,1,1,1,1)
+	return t
 }
-func (t *PortT) PullUp(v ...uint8) {
+
+// PullUp プルアップ
+func (t *PortT) PullUp(v ...uint8) *PortT {
 	t.p.Write(t.addr.gppu, S2BV(v))
+	return t
 }
-func (t *PortT) PullUpAll() {
+
+// PullUpAll すべてのピンをプルアップにする
+func (t *PortT) PullUpAll() *PortT {
 	t.PullUp(1,1,1,1,1,1,1,1)
+	return t
 }
-func (t *PortT) Apply() {
+
+// Apply 反映
+func (t *PortT) Apply() *PortT {
 	t.p.Write(t.addr.olat, t.State)
+	return t
 }
-func (t *PortT) Fetch() {
+
+// Fetch 取得
+func (t *PortT) Fetch() *PortT {
 	t.State = t.p.Read(t.addr.gpio)
+	return t
 }
-func (t *PortT) SetAllLow(a bool) {
+
+// SetAllLow すべてLowにする
+func (t *PortT) SetAllLow() *PortT {
 	t.State = 0x00
-	if a {
-		t.Apply()
-	}
+	return t
 }
-func (t *PortT) SetAllHigh(a bool) {
+
+// SetAllHigh すべてHighにする
+func (t *PortT) SetAllHigh() *PortT {
 	t.State = 0xFF
-	if a {
-		t.Apply()
-	}
+	return t
 }
-func (t *PortT) Set(p byte, v bool, a bool) {
+
+// Set 設定
+func (t *PortT) Set(p byte,v bool) *PortT {
 	if v {
 		t.State = t.State |  ( 1 << p )
 	} else {
 		t.State = t.State &^ ( 1 << p )
 	}
-	if a {
-		t.Apply()
-	}
+	return t
 }
 
+// Get 取得
 func (t *PortT) Get(p byte) bool {
 	if t.State & ( 1 << p ) > 0 {
 		return true
@@ -163,8 +190,9 @@ func (t *PortT) Get(p byte) bool {
 	}
 }
 
-func (t *PortT) InitInterrupt() {
-	// 以下、すべてのピンに対して設定する
+// InitInterrupt 割り込み初期化
+func (t *PortT) InitInterrupt() *PortT {
+	// すべてのピンに対して設定する
 	// INTnピンはデフォルトはアクティブLow
 	// IOCON.INTPOLで変更できる
 
@@ -174,5 +202,43 @@ func (t *PortT) InitInterrupt() {
 	// 前の状態と比較する
 	t.p.Write(t.addr.intcon, 0x00)
 
+	return t
+}
+
+// PinT ポートとピンをインスタンス化する
+type PinT struct {
+	Port *PortT
+	pin  uint8
+}
+
+// NewPin 新規PinT
+func NewPin(port *PortT, pin uint8) *PinT {
+	t := new(PinT)
+	t.Port = port
+	t.pin  = pin
+	return t
+}
+
+// Apply 反映
+func (t *PinT) Apply() *PinT {
+	t.Port.Apply()
+	return t
+}
+
+// Fetch 取得
+func (t *PinT) Fetch() *PinT {
+	t.Port.Fetch()
+	return t
+}
+
+// Set 適用
+func (t *PinT) Set(v bool) *PinT {
+	t.Port.Set(t.pin,v)
+	return t
+}
+
+// Get 取得
+func (t *PinT) Get() bool {
+	return t.Port.Get(t.pin)
 }
 
